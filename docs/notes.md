4c7e9925aea0a4f058baf2c1f3918534520bf7c5
Abdou990F <benari102@gmail.com>
2023-09-10 18:47:52 +0100

    Added lots of stuff * Terminal Updating * Fixed `move_block` * Multiple Blocks moving

---
+++ docs/notes.md
@@ -0,0 +1,7 @@
+# Notes
+empty...
+
+# Dev Notes
+
+* In the code *(in `for` loops especially)*, `i` is often used by me for checking **X positions**,
+and `j` for checking **Y positions**.
--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 """ Original Plain Text Tetris Game By @AbdooOwd inspired by Alexey Pijatnov """
 
-#* All this project was all made without ChatGPT, except for a little bit of the code
+#* All this project was all made without ChatGPT, except for a little bit of the code (like 2%)
 
+import os
+import time
 
 class GameLayout:
 
@@ -29,40 +31,51 @@ class GameLayout:
         self.es: str = slots[0]
         self.block: str = slots[1]
         self.borders: list = borders
-
+        
+        # Event handling
+        self.event_occured = True # Originally True to let the game start at least
 
         #* To edit blocks
         self.block_slots:  list = [ [], [] ]
         self.empty_slots: list = [ [], [] ]
-        
+
         for j in range(self.rows):
             self.empty_slots.append([])
             self.block_slots.append([])
-        
+
         self.isLastBlock : bool = False # To check if the last placed slot is a block
 
         #! The grid/GAME
         self.layout: str = ""
+    
+    def clear_terminal(self):
+        """Clears the terminal"""
+        os.system('cls' if os.name == 'nt' else 'clear')
+    
+    def update_display(self):
+        """Updates and displays the game layout in the terminal"""
+        self.clear_terminal()
+        print(self.generate())
 
     def reset_slots(self):
         #* To edit blocks
         self.block_slots:  list = [ [], [] ]
         self.empty_slots: list = [ [], [] ]
-        
+
         self.isLastBlock : bool = False # To check if the last placed slot is a block
 
     def generate(self):
         """Generates a Game Board/View/Grid"""
-        
-        print(f" > 'block_slots' : {self.block_slots}") #! DEV
-        print(f" > 'empty_slots' : {self.empty_slots}")
+
+        # print(f" > 'block_slots' : {self.block_slots}") #! DEV
+        # print(f" > 'empty_slots' : {self.empty_slots}")
 
         # Column Storing Stuff
         self.column_list: list = []
 
         for j in range(self.rows):
             self.column_list.append([])
-            
+
             # Check if 'block_slots' and 'empty_slots' have the same positions
             for i in range(self.columns):
                 if self.block_slots != [[], []] and self.empty_slots != [[], []] and i <= len(self.block_slots):
@@ -83,7 +96,7 @@ class GameLayout:
                     continue
 
                 self.column_list[j].append(self.es)
-        
+
 
         self.str_h: list = []
 
@@ -120,26 +133,24 @@ class GameLayout:
         * `where` : Index - 0: int for 1 column - 1: list for one or multiple rows
         """
 
-        for i in where[1]: #Checks if already in the Array
-            if i in self.block_slots[where[0]]:
-                return
-        
-        self.block_slots[where[0]].extend(where[1])
-        self.isLastBlock = True
-    
-    
+        # Add the block to its new position
+        for i in where[1]:
+            self.block_slots[where[0]].append(i)
+            if where[0] in self.empty_slots[i]:
+                self.empty_slots[i].remove(where[0])
+
+
     def set_empty(self, where:list=[0,[]]):
         """
         Add blocks on column(s)
         * `where` : Index - 0: int for 1 column - 1: list for one or multiple rows
         """
         
+        # Remove the block from its current position
         for i in where[1]:
-            if i in self.empty_slots[where[0]]:
-                return
-
-        self.empty_slots[where[0]].extend(where[1])
-        self.isLastBlock = False
+            self.empty_slots[i].append(where[0])
+            if where[0] in self.block_slots[i]:
+                self.block_slots[i].remove(where[0])
 
 
     def add_tetro(self, tetro:int=0, x:int=999):
@@ -147,7 +158,7 @@ class GameLayout:
         """
         Add Tetrominos based on the given value for `tetro`
         * `tetro` : `0`: 2x2 - `1`: 1x4 - `2`: S - `3`: 5 - `4`: T
-        * `x` : The X position, by default its the center (columns number / 2 rounded) 
+        * `x` : The X position, by default its the center (columns number / 2 rounded)
         """
 
         if x == 999:
@@ -180,30 +191,48 @@ class GameLayout:
             case _:
                 pass
 
-
-    def move_block(self, slot:list[int]=[0,0], move:list[int]=[0, 0]):
+    def move_block(self, slot: list[int] = [0, 0], move: list[int] = [0, 0]):
         """
-        Move a slot base on the given values
+        Move a block based on the given values
         * `slot` : Indexes -> `0`: X pos - `1`: Y pos
         * `move` : Where to move - Indexes -> `0`: X pos - `1`: Y pos
         """
+        
+        if self.empty_slots
 
-        for j in range(self.rows):
-            for i in range(self.columns):
-                if j == slot[1] and i == slot[0]:
-                    self.set_empty([j, [i]])
+        # Remove the block from its current position
+        self.set_empty([slot[0], [slot[1]]])
 
-                if j == move[1] and i == move[0]:
-                    self.set_slots([j, [i]])
+        # Add the block to its new position
+        self.set_slots([move[0], [move[1]]])
 
 
-    def move_slots(self): # TODO: Add this
+    def gravity_block(self):
         pass
+
+
+    def move_slots(self, slots: list[list[int]] = [[], []], move: list[list[int]] = [[], []]):
+        # TODO: Add this
+        """
+        Moves multiples slots
+        * `slots` : Indexes ->
+        `0`: List of X positions - 
+        `1`: List of Y positions
+        * `move` : Indexes ->
+        `0`: List of X positions - 
+        `1`: List of Y positions
+        """
+        
+        for i in range(len(slots[0])):
+            self.move_block([slots[0][i], slots[1][i]], [move[1][i], move[0][i]])
+            
+            # print(f" [DEV] >> Moved Slot:\n * X: {slots[0][i]}\n * Y: {slots[1][i]}")
+            # print(f" - - >> To; \n * X: {move[0][i]}\n * Y: {move[1][i]}")
 
 
     def show(self):
         print(self.generate())
-    
+
 
     def __str__(self) -> str:
         if self.layout == "":
@@ -213,17 +242,38 @@ class GameLayout:
 
 
 
+if __name__ == "__main_caca__":
+    tetris = GameLayout()
+    
+    tetris.add_tetro(0, 2)
+
+    # Set up the game loop
+    while True:
+        # Update and display the game layout
+        tetris.update_display()
+
+        # Check if the player pressed a key for left or right movement
+        player_input = input("Press 'L' for left, 'R' for right, or 'Q' to quit: ")
+        # if player_input.lower() == 'l':
+        #     tetris.move_block_left()
+        # elif player_input.lower() == 'r':
+        #     tetris.move_block_right()
+        if player_input.lower() == 'q':
+            break  # Exit the game loop if the player quits
+
+        # Implement gravity by adding a delay (adjust sleep time as needed)
+        time.sleep(1)  # Sleep for 1 second, adjust as needed
+
+        # Move the tetromino down
+        tetris.gravity_block()
+
+        # TESTIT: Gravity by taking the slot X pos and Then (Y - 1) Will be below's slot's X
+
 if __name__ == "__main__":
-    tetris = GameLayout(row_num=12, col_num=5)
+    tetris_dev = GameLayout()
     
-    tetris.add_tetro(0)
-    tetris.show()
+    tetris_dev.add_tetro(0, 2)
+    tetris_dev.show()
     
-    tetris.set_empty([0,[2]])
-    tetris.show()
     
-    tetris.set_slots([0, [2]])
-    tetris.show()
-
-    # TESTIT: Gravity by taking the slot X pos and Then (Y - 1) Will be below's slot's X
-
+    tetris_dev.show()
